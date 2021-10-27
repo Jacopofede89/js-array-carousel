aead2770ba88362c3cf4fde8c4f502b3b99de0f9
Jacopofede89 <jacopofede89@gmail.com>
2021-10-28 00:49:21 +0200

    finale

--- js/script.js
+++ js/script.js
@@ -27,15 +27,16 @@ const text = [
     'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
 ]
 
+// selezioni elemanti
 
 const itemsCont = document.querySelector(".items");
 const thumbsCont = document.querySelector(".thumbs");
 const next = document.querySelector(".next");
-const prev = document.querySelector(".next");
-
-
+const prev = document.querySelector(".prev");
 let item = "";
 let thumbs= "";
+
+// output elementi
 
 for( let i = 0; i < items.length; i++){
     item += `
@@ -52,12 +53,12 @@ for( let i = 0; i < items.length; i++){
     <img src="${items[i]}" alt="">
     </div>`;
 
-}
+};
 
 itemsCont.innerHTML= item;
 document.getElementsByClassName("item")[0].classList.add("active");
 thumbsCont.innerHTML= thumbs;
-document.getElementsByClassName("thumbs")[0].classList.add("active");
+document.getElementsByClassName("thumb")[0].classList.add("active");
 
 let activeAct = 0;
 
@@ -65,8 +66,23 @@ next.addEventListener("click",
 
 function() {
     ++activeAct;
-    document.getElementsByClassName("thumbs")[activeAct].classList.add("active");
-    document.getElementsByClassName("thumbs")[activeAct].classList.add("active");
+    document.querySelector(".item.active").classList.remove("active");
+    document.getElementsByClassName("item")[activeAct].classList.add("active");
+    document.querySelector(".thumb.active").classList.remove("active");
+    document.getElementsByClassName("thumb")[activeAct].classList.add("active");
+
+}
+
+);
+
+prev.addEventListener("click",
+
+function() {
+    --activeAct;
+    document.querySelector(".item.active").classList.remove("active");
+    document.getElementsByClassName("item")[activeAct].classList.add("active");
+    document.querySelector(".thumb.active").classList.remove("active");
+    document.getElementsByClassName("thumb")[activeAct].classList.add("active");
 }
 
 );
